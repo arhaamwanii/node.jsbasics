06a895f545193c1679bdde11bdbb7520f0bd8988
arhaamwanii <arhamwani765@gmail.com>
2024-04-02 21:26:00 +0530

    basic

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+
+http.createServer((req , res) => {
+
+});
+
+
+// takes  a request listner as an argument
+// this argument will be executed for every time someone does an request
+    // then this fucntion will recieve two aruments 
+    // first one : will cotain data about the request
+    // second one : will help to send the response
